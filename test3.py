98d0c8467047d099dbd74efe14b9a8f81c924cff
BYXY <165575337+BYXY01@users.noreply.github.com>
2026-07-30 01:04:00 +0800

    add v3

--- test3.py
+++ test3.py
@@ -26,21 +26,17 @@ def tongji3(cut_res:list): # 统计词和词之间的距离
 
 def jiaquan2(input_words:list, word_dic1:dict, word_dic2:dict, word_dic3:dict):
     points=[0,0.5,0.25,0.25]
-    dic1_plus_res,dic2_plus_res,dic3_plus_res={},{},{}
-    res_dic={}
+    t_dic={}
     for iwd in input_words:
         for k,v in word_dic1.get(iwd,{}).items():
-            dic1_plus_res[k]=dic1_plus_res.get(k,0)+v
-            res_dic[k]=0
+            t_dic.setdefault(k,[0,0,0,0])[1]+=v
         for k,v in word_dic2.get(iwd,{}).items():
-            dic2_plus_res[k]=dic2_plus_res.get(k,0)+v
-            res_dic[k]=0
+            t_dic.setdefault(k,[0,0,0,0])[2]+=v
         for k,v in word_dic3.get(iwd,{}).items():
-            dic3_plus_res[k]=dic3_plus_res.get(k,0)+(1/v[0])
-            res_dic[k]=0
-    for k in res_dic.keys():
-        res_dic[k] = (dic1_plus_res.get(k,0) * points[1]) + (dic2_plus_res.get(k,0) * points[2]) + (dic3_plus_res.get(k,0) * points[3])
-    res_lst = test2.dic_fast_sorted(res_dic)
+            t_dic.setdefault(k,[0,0,0,0])[3]+=1/v[0]
+    for v in t_dic.values():
+        v[0]=sum(v[i]*points[i] for i in range(1,4))
+    res_lst=sorted(t_dic.items(),key=lambda d:d[1][0],reverse=True)
     return res_lst[:len(res_lst)//2]
 
 def jiaquan_output_test2(words,ignor_words=''):
